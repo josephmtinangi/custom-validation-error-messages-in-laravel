47d464802eae9b5d160e5c847b095c160ac87079
Joseph Mtinangi <josephmtinangi@gmail.com>
2017-02-09 12:09:51 +0300

    Validate form

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -19,7 +19,13 @@ class ContactController extends Controller
     		'message' => 'required|min:5',
     	];
 
-    	$this->validate($request, $rules);
+			$customMessages = [
+			    'name.required' => 'Yo, what should I call you?',
+			    'email.required' => 'We need your email address also',
+			    'message.required'  => 'c\'mon, you want to contact me without saying anything?',
+			 ];
+
+    	$this->validate($request, $rules, $customMessages);
 
     	return back()->with('status', 'Your message has been received, We will get back to you shortly.');
     }
